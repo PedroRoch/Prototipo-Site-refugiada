698ad534b1dc1d7d915c8dec3a3c0f8f278cc147
PedroRoch <103005263+PedroRoch@users.noreply.github.com>
2022-06-07 08:54:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+/* pagina inicial */
+function abrigar(){
+  window.open("procuro_abrigar.html", "_self")
+};
+
+function refugiada(){
+  window.open("refugio.html", "_self")
+};
+
+function logar(){
+  window.open("tela_login.html", "_self")
+};
+
+function inicio(){
+  window.open("index.html", "_self")
+};
